d759c08c6cad24ae2e7f45571d9104566a390cda
Freddier <hch.fredy@gmail.com>
2023-06-15 16:54:45 -0500

    9na Clase-Buscando TODO's

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { CreateTodoButton } from './CreateTodoButton';
 
 const defaultTodos=[
   {text:'Aprender GitHub',completed:true},
-  {text:'Aprender HTML5',completed:false},
+  {text:'Aprender HTML5',completed:true},
   {text:'Aprender Tailwind3',completed:false},
   {text:'Aprender JavaScript',completed:false},
   {text:'Curso Profesional de Git y GitHub',completed:true},
@@ -16,28 +16,39 @@ const defaultTodos=[
 function App() {
   const [todos]=React.useState(defaultTodos);
   const [searchValue,setSearchValue]=React.useState('');
-
-  // LOS ESTASOS DERIVADSO: Son propiedades, variables, cálculos que hacemos a partir de un estado.
-  const completedTodos = todos.filter(todo=>!!todo.completed).length;
+  
+  // LOS ESTASOS DERIVADDOS: Son propiedades, variables, cálculos que hacemos a partir de un estado.
+  const completedTodos = todos.filter(todo => !!todo.completed).length; // !! - retorna un boleano
   const totalTodos=todos.length;
-    console.log('Los usuarios buscan todos de '+ searchValue);
-  return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos}/>
-  <TodoSearch
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-  />
+  // Filtrar datos en el buscador
+  const searchedTodos = todos.filter(
+    (todo)=> {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
 
+    console.log(`Buscando todos de ${searchValue}`);
+
+  return (
+    <React.Fragment>
+      <TodoCounter completed={completedTodos} total={totalTodos}/>
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+          {searchedTodos.map(todo => (
+          <TodoItem 
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}/>
         ))}
 
       </TodoList>
 
       <CreateTodoButton/>
-    </>
+    </React.Fragment>
   );
 }
 
