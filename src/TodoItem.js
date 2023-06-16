8df0e57d6897a15c9e0659b6ca0d881a14bf780c
Freddier <hch.fredy@gmail.com>
2023-06-15 20:21:09 -0500

    12va Clase-Iconos con Colores Dinámicos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,13 @@
 import {CompleteIcon} from './CompleteIcon';
 import {DeleteIcon} from './DeleteIcon';
 import './TodoItem.css';
+
 function TodoItem(props){
     return(
         <li className="TodoItem" >
-            <CompleteIcon/>
-            {/* <span className={`Icon Icon-check ${props.completed && "Icon-check--active"} `} onClick={props.onComplete} >✔</span> */}
+            <CompleteIcon completed={props.completed} onComplete={props.onComplete}/>
             <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-            <DeleteIcon/>
-            {/* <span className="Icon Icon-delete" onClick={props.onDelete}>x</span> */}
+            <DeleteIcon onDelete={props.onDelete}/>
         </li>
     )
 }
