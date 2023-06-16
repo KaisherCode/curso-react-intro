45b199a4a2cdcb3ea03cd674e2e7a06b5ceef48c
Freddier <hch.fredy@gmail.com>
2023-06-16 10:47:29 -0500

    19va Clase-Estados de Carga y Error | 20va Clase-Actualizando Estados desde useEffect

--- src/App/index.js
+++ src/App/index.js
@@ -3,8 +3,7 @@ import { AppUI } from './AppUI';
 import {useLocalStorage} from './useLocalStorage';
 
 function App() {
-  
-  const [todos,saveTodos]=useLocalStorage('TODOS_V1',[]);
+  const {item:todos,saveItem:saveTodos,loading,error}=useLocalStorage('TODOS_V1',[]);
   const [searchValue,setSearchValue]=React.useState('');
   
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -36,6 +35,8 @@ function App() {
 
     return(
       <AppUI
+      loading={loading}
+      error={error}
       completedTodos={completedTodos}
       totalTodos={totalTodos}
       searchValue={searchValue}
