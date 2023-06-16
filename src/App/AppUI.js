b0fc7b56f0480f6403a1a6874e696ebabac95bb8
Freddier <hch.fredy@gmail.com>
2023-06-16 11:53:26 -0500

    21va Clase-Loading Skeletons

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,6 +2,9 @@ import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
+import {TodosLoading} from '../TodosLoading';
+import {TodosError} from '../TodosError';
+import {TodosEmpty} from '../TodosEmpty';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({
@@ -24,9 +27,9 @@ function AppUI({
             setSearchValue={setSearchValue}
             />
             <TodoList>
-                {loading && <p>Estamos cargando...</p>}
-                {error && <p>Hubo un error!!</p>}
-                {(!loading && searchedTodos.length===0) && <p>!Crea tu primer TODO!</p>}
+                {loading && (<><TodosLoading/><TodosLoading/><TodosLoading/><TodosLoading/></>)}
+                {error && <TodosError/>}
+                {(!loading && searchedTodos.length===0) && <TodosEmpty/>}
 
                 {searchedTodos.map(todo => (
                 <TodoItem 
---
+++ src/TodosEmpty/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import './TodosEmpty.css';
+
+function TodosEmpty(){
+    
+    return(
+        <p>¡Crea tu primer TODO!</p>
+    )
+}
+export {TodosEmpty};
